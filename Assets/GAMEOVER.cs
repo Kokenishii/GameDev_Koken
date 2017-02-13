d061eabad2e5d9edba8e78604d4e6eaeca2d1539
Unknown <ki575@nyu.edu>
2017-02-13 13:29:04 -0500

    1. GameOver screen 2. Restart function 3. A playable level with: jump > umbrella > wind > gravity change > slider(obstacle) > game over > restart

--- Assets/GAMEOVER.cs
+++ Assets/GAMEOVER.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GAMEOVER : MonoBehaviour {
-
+    public Text gameOver;
+    bool gameOverStatus = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,19 @@ public class GAMEOVER : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(gameOverStatus)
+        {
+            gameOver.gameObject.SetActive(true);
+        }
 	}
+   void OnTriggerEnter2D()
+    {
+        gameOverStatus = true;
+      
+
+    }
+    void OnTriggerExit2D()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
 }
